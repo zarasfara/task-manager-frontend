a2f548af6c0702aeb0e08b2dd17fd1557ad756d4
zarasfara <oev2001@gmail.com>
2023-05-12 19:32:01 +0500

    added: vue router and showing home page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
-<script setup>
-import Header from './components/Header.vue';
+<script>
+import AppHeader from './components/Header.vue';
 import SideBar from "./components/SideBar.vue"
+
+export default {
+  name: "Layout",
+  components: {
+    AppHeader,
+    SideBar
+  }
+}
 
 </script>
 
 <template>
-  <Header />
+  <app-header />
   <div class="desktop:container mx-auto flex md:flex-row sm:flex-col mt-4">
-    <section
-      class="main-block desktop:w-4/5 md:w-2/3 md:mr-2 desktop:mr-2 bg-white rounded-md py-2.5 sm:mt-4 px-3 border border-gray-200 sm:order-2 drop-shadow-md">
-      <slot></slot>
+    <section class="main-block desktop:w-4/5 md:w-2/3 md:mr-2 desktop:mr-2 bg-white rounded-md
+                        py-2.5 sm:mt-4 px-3 border border-gray-200 sm:order-2 drop-shadow-md">
+      <router-view></router-view>
     </section>
     <side-bar />
   </div>
 </template>
+
 
 <style scoped></style>
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from './views/Home.vue';
+
+
+export default createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        }
+    ]
+})
